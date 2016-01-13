71fa802b525df4086fa9b139bc12bc604cf9c15b
Cosmin Atomei <cosmin.atomei@gmail.com>
2016-01-13 16:48:55 +0200

    REST API Order/Save full integration & tests

--- webhooks/orders/create.php
+++ webhooks/orders/create.php
@@ -1,5 +1,9 @@
 <?php
 	require __DIR__.'/../../db.php';
+
+	require __DIR__.'/../../vendor/autoload.php';
+	use phpish\shopify;
+
 	require __DIR__.'/../../conf.php';
 	require __DIR__.'/../../lib/retargeting-rest-api/Client.php';
 
@@ -11,50 +15,57 @@
 	$hmac_header = $_SERVER['HTTP_X_SHOPIFY_HMAC_SHA256'];
 	$data = file_get_contents('php://input');
 	$verified = verify_webhook($data, $hmac_header);
-	
-	// $file = "\n\n=========".print_r($verified, true)."::".print_r($_SERVER, true)."=========\n\n".print_r($data, true);
-	// $file .= file_get_contents(__DIR__.'/log.txt');
-	
-	// file_put_contents(__DIR__.'/log.txt', $file);
 
-	// echo "<pre>".file_get_contents(__DIR__.'/log.txt')."</pre>";
+	if ($verified && !empty($_SERVER['HTTP_X_SHOPIFY_SHOP_DOMAIN']) && $shop = verifyShopStatus($conn, $_SERVER['HTTP_X_SHOPIFY_SHOP_DOMAIN'])) {
 
-
-	if ($verified && !empty($_SERVER['HTTP_X_SHOPIFY_SHOP_DOMAIN'])) {
-
-		$shop = getShopDetails($conn, $_GET['shop']);
+		$shopify = shopify\client($shop['shop'], SHOPIFY_APP_API_KEY, $shop['token']);
 
 		if (empty($shop['domain_api_key']) || empty($shop['api_token']))
 			return false;
 		
-
-		// get Order through Shopify API
-
+		// get Order through Shopify API - unsolved issue
+		// $order = $shopify('GET /admin/orders.json?ids='.$data['id']);
 
 		$client = new Retargeting_REST_API_Client($shop['domain_api_key'], $shop['api_token']);
 		$client->setResponseFormat("json");
 		$client->setDecoding(false);
 		
+		$discountCodes = array();
+		foreach ($data['discount_codes'] as $dc) {
+			$discountCodes[] = $dc['code'];
+		}
+		$discountCodes = implode(', ', $discountCodes);
+
+		$lineItems = array();
+		foreach ($data['line_items'] as $li) {
+			$lineItems[] = array(
+				'id' => $li['id'],
+				'quantity' => $li['quantity'],
+				'price' => $li['price'],
+				'variation_code' => $li['variant_id']
+			);
+		}
+
 		$response = $client->order->save(
 			array(
 				'order_no' => $data['order_number'],
-				'lastname' => 'Doe',
-				'firstname' => 'John',
-				'email' => 'johndoe@mail.com',
-				'phone' => '0700555888',
-				'state' => 'Dambovita',
-				'city' => 'Targoviste',
-				'address' => 'Bd. Independentei',
-				'discount' => 20,
-				'discount_code' => 'RAX204',
-				'shipping' => 19,
-				'total' => 199
+				'lastname' => '',
+				'firstname' => '',
+				'email' => $data['email'],
+				'phone' => '',
+				'state' => '',
+				'city' => '',
+				'address' => '',
+				'discount' => $data['total_discounts'],
+				'discount_code' => $discountCodes,
+				'shipping' => 0,
+				'total' => $data['total_price']
 				),
-			array(
-				array('id' => 307, 'quantity' => 1, 'price' => 18, 'variation_code' => ''),
-				array('id' => 48, 'quantity' => 2, 'price' => 300, 'variation_code' => 'R-S'),
-				array('id' => 48, 'quantity' => 1, 'price' => 300, 'variation_code' => 'Y-S'),
-				array('id' => 50, 'quantity' => 1, 'price' => 22, 'variation_code' => 'Y-S')
-				)
+			$lineItems
 		);
+
+		// $file = print_r($response, true)."\n\n***********************\n\n".file_get_contents(__DIR__.'/log.txt');
+		// file_put_contents(__DIR__.'/log.txt', $file);
 	}
+
+	echo "<pre>".file_get_contents(__DIR__.'/log.txt')."</pre>";
