2e3233f8a18d4e3056a54ddc6954655536e36142
Cornel Bratu <bratucornel@users.noreply.github.com>
2016-11-20 19:42:29 +0200

    Update oauth.php

--- oauth.php
+++ oauth.php
@@ -14,7 +14,27 @@
 	# Step 2: http://docs.shopify.com/api/authentication/oauth#asking-for-permission
 	if (!isset($_GET['code']))
 	{
-		$permission_url = shopify\authorization_url($_GET['shop'], SHOPIFY_APP_API_KEY, array('read_content', 'write_content', 'read_themes', 'write_themes', 'read_products', 'write_products', 'read_customers', 'write_customers', 'read_orders', 'write_orders', 'read_script_tags', 'write_script_tags', 'read_fulfillments', 'write_fulfillments', 'read_shipping', 'write_shipping'));
+		$permission_url = shopify\authorization_url(
+			$_GET['shop'], SHOPIFY_APP_API_KEY, 
+			array(
+				'read_content', 
+				'write_content', 
+				'read_themes', 
+				'write_themes', 
+				'read_products', 
+				'write_products', 
+				'read_customers', 
+				'write_customers', 
+				'read_orders', 
+				'write_orders', 
+				'read_script_tags', 
+				'write_script_tags', 
+				'read_fulfillments', 
+				'write_fulfillments', 
+				'read_shipping', 
+				'write_shipping'
+			)
+		);
 		die("<script> top.location.href='$permission_url'</script>");
 	}
 
@@ -23,7 +43,11 @@
 	try
 	{
 		# shopify\access_token can throw an exception
-		$oauth_token = shopify\access_token($_GET['shop'], SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, $_GET['code']);
+		$oauth_token = shopify\access_token(
+			$_GET['shop'], 
+			SHOPIFY_APP_API_KEY, 
+			SHOPIFY_APP_SHARED_SECRET, 
+			$_GET['code']);
 
 		$_SESSION['oauth_token'] = $oauth_token;
 		$_SESSION['shop'] = $_GET['shop'];
